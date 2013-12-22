e69b955df855708b218ca95526aebff058e6671c
nicolas labbe <nicolas.labbe@adfab.fr>
2013-12-22 18:58:53 +0100

    LABSLOOQ-50

--- js/background.js
+++ js/background.js
@@ -10,6 +10,10 @@ if(v.tooluxversion !== details.version) {
 */
 
 //chrome.tabs.executeScript(null, {file:"http://localhost:1337/socket.io/socket.io.js"});
+var severUrl = 'looq.fr', nodeUrl = '46.105.99.216:1337';
+//var severUrl = 'looq.livedemo.fr', nodeUrl = '127.0.0.1:1337';
+//var severUrl = 'ic.adfab.fr/looq', nodeUrl = '127.0.0.1:1337';
+//var severUrl = 'looq.server', nodeUrl = '127.0.0.1:1337';
 
 var background = {
 
@@ -29,10 +33,10 @@ var background = {
     {
         'use strict';
         
-        chrome.tabs.create({'url': 'http://looq.fr/'}, function (tab)
+        //chrome.tabs.create({'url': 'http://looq.fr/'}, function (tab)
         //chrome.tabs.create({'url': 'http://looq.livedemo.fr/'}, function (tab)
         //chrome.tabs.create({'url': 'http://ic.adfab.fr/looq/'}, function (tab)
-        //chrome.tabs.create({'url': 'http://looq.server/'}, function (tab)
+        chrome.tabs.create({'url': 'http://' + severUrl + '/'}, function (tab)
         {
             // Tab opened.
         });
@@ -43,7 +47,7 @@ var background = {
         'use strict';
         
         var notif = webkitNotifications.createNotification(
-            'icon100.png',  // icon url - can be relative
+            'icon48.png',  // icon url - can be relative
             data.title,  // notification title
             data.message
         );
@@ -81,7 +85,7 @@ chrome.extension.onRequest.addListener(
     }
 );
 
-var socket = io.connect('http://localhost:1337/looq');
+var socket = io.connect('http://' + nodeUrl + '/looq');
 socket.on('connect', function (data)
 {
     socket.emit('login', {name: 'nicolas.labbe@adfab.fr'});
