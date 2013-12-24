75bb72813719dbbc6e713567e0cb4a6f0f5b653d
nicolaslabbe <nicolas.labbe@adfab.fr>
2013-12-24 15:13:47 +0100

    LOOQ-1

--- js/background.js
+++ js/background.js
@@ -10,7 +10,7 @@ if(v.tooluxversion !== details.version) {
 */
 
 //chrome.tabs.executeScript(null, {file:"http://localhost:1337/socket.io/socket.io.js"});
-var severUrl = 'looq.fr', nodeUrl = '46.105.99.216:8889';
+var severUrl = 'looq.fr', nodeUrl = '46.105.99.216:8080';
 //var severUrl = 'looq.livedemo.fr', nodeUrl = '127.0.0.1:8889';
 //var severUrl = 'ic.adfab.fr/looq', nodeUrl = '46.105.121.40:8889';
 //var severUrl = 'looq.server', nodeUrl = '127.0.0.1:8889';
