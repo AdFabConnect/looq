f4c6173973382ba9ff4bff8d1fcda888913f3706
nicolas labbe <nicolas.labbe@adfab.fr>
2014-01-14 21:00:20 +0100

    LABSLOOQ-76

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-//var severUrl = 'www.looq.fr', nodeUrl = '46.105.99.216:8080';
+var severUrl = 'www.looq.fr', nodeUrl = '46.105.99.216:8080';
 //var severUrl = 'looq.livedemo.fr', nodeUrl = '127.0.0.1:8889';
 //var severUrl = 'ic.adfab.fr/looq', nodeUrl = '46.105.121.40:8889';
-var severUrl = 'looq.server', nodeUrl = '127.0.0.1:8889';
+//var severUrl = 'looq.server', nodeUrl = '127.0.0.1:8889';
